b18ca09bd5c5bedcfd2fcc395015317560037d8b
littlegamer757 <jneissl27@gmail.com>
2022-01-24 11:08:54 +0100

    Update yaxis
    keine Ahnung wos des duad, glaub des zoagt s Ergebnis glei on oda sowos

--- chartreader/yaxis.py
+++ chartreader/yaxis.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("../docs/Beispiele/Run 9/00.0-08.0-35.0-35.0-40.0-30.0-01.0-04.0-02.0-NONE.png")
+img = cv2.imread('../docs/Beispiele/Run 9/00.0-08.0-35.0-35.0-40.0-30.0-01.0-04.0-02.0-NONE.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
@@ -23,6 +23,8 @@ for r, theta in lines[0]:
 
 cv2.imwrite('test.jpg', img)
 
-# cv2.imshow("Seas Jungs", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+img = cv2.imread('test.jpg')
+cv2.imshow('Line detection result', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+exit
