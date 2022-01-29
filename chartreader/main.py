bc56507ae74d3aeb4e6cc02b7df65a89a49ea37d
ungarmichael <ungarmichael.mail@gmail.com>
2022-01-29 14:51:32 +0100

    add chartcoo

--- chartreader/main.py
+++ chartreader/main.py
@@ -5,19 +5,22 @@ import cv2 as cv
 
 def init():
     global img
+    global chartCoo
     img = cv.imread('./input/1.png')
 
 def fetchBluestByProximity():
-    for columnIndex in range(len(img[1])):
-        n, i = Chart.readTheBluestValue(img[:,columnIndex])
-        img[i,columnIndex] = [150,120,200] # img [y,x]
+    chartCoo = []
+    for xColumnIndex in range(len(img[1])):
+        n, yBluestIndex = Chart.readTheBluestValue(img[:,xColumnIndex])
+        img[yBluestIndex,xColumnIndex] = [150,120,200] # img [y,x]
+        chartCoo.append([xColumnIndex, yBluestIndex]) # chartVal [x, y]
+    # print(chartCoo)
     cv.imshow('test1',img)
     cv.waitKey(0)
 
 def main():
     init()
     fetchBluestByProximity()
-    
 
 if __name__ == "__main__":
     main()
