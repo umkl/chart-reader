df3513076a45ef3b102ce4313d4bff0f2841a2b2
littlegamer757 <jneissl27@gmail.com>
2022-02-12 15:02:04 +0100

    Begin y-axis detection

--- chartreader/yaxis.py
+++ chartreader/yaxis.py
@@ -1,4 +1,5 @@
-from const import * 
+from const import *
+import cv2
 
 class LogAxis:
     """
@@ -32,17 +33,30 @@ class LogAxis:
 
     # def defineValues():
     #     for i in range(1000):
-            
+
+img = cv2.imread('../docs/Beispiele/Run 9/00.0-08.0-35.0-35.0-40.0-30.0-01.0-04.0-02.0-NONE.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray.jpg', gray)
+
+# originYPos = -72
+# originXPos = 103
+# originHexVal = '#b0b0b0'
+# while (originXPos < 0)
+#
+#
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+
 # 200 von xachse
 # 255 * log()
 # 255*log(wert)=y
 # wert= 10^((1000+y)/255)
 # wert = 10^((756+y)/255)
 
-# 0 = 648 
+# 0 = 648
 # 1 = 639
 # 2 = 384
 # d = 264
-# 9 
+# 9
 
 # x = 100
