c29b81d6e5208983f7397f683de2df629feab91d
littlegamer757 <jneissl27@gmail.com>
2022-03-09 11:56:06 +0100

    Commit
    I woaß nimma, miassat scho passn

--- chartreader/yaxis.py
+++ chartreader/yaxis.py
@@ -1,6 +1,9 @@
 import os
 import cv2
 
+# TODO (jonik): Lea braucht nur Striche bei de Zoihn
+# Spalte links von da y-Achse von unten durchgeh, Obstände zwischen graue Pixel (Logarithmus-Striche) messen.
+# Soboid da Obstond greßa is ois da vorherige, wiss ma, dass do a Zoih daneben steht und speichern den y-Wert von dem Strich fiad Lea.
 
 class LogAxis:
     """
@@ -11,13 +14,14 @@ class LogAxis:
         valuesNoOffset {[number, number]} -- list for log-values on each row/y-axis or preciser with no Offset (according to pixel values on image)
     """
 
-    originHeight = -72;
-    originGrayVal = 178;
+    originHeight = -72
+    originGrayVal = 178
 
-    def __init__(self, img):
-        self.__img = img
-        self.__values = __getYAxisValuesOffset(img)
-        self.__valuesNoOffset = __getYAxisValues(img)
+    def __init__(self, imgPath):
+        self.__img = cv2.imread(imgPath)
+        self.__imgPath = imgPath
+        self.__values = self.getYAxisValuesOffset()
+        self.__valuesNoOffset = self.getYAxisValues()
 
     def setValues(self, val):
         self.__values = val
@@ -41,35 +45,35 @@ class LogAxis:
 
     valuesNoOffset = property(fget=getValuesNoOffset, fset=setValuesNoOffset, fdel=delValuesNoOffset, doc=None)
 
-    def __getOriginXPos(img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def getOriginXPos(self):
+        gray = cv2.cvtColor(self.__img, cv2.COLOR_BGR2GRAY)
         width = gray.shape[1]
         origin_x_pos = -1
         for i in range(width):
-            if gray[originHeight, i] == originGrayVal:
+            if gray[self.originHeight, i] == self.originGrayVal:
                 origin_x_pos = i
 
         return origin_x_pos
 
-    def __getYAxisValues(img):
-        origin_x_pos = __getOriginXPos(img)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def getYAxisValues(self):
+        origin_x_pos = self.getOriginXPos()
+        gray = cv2.cvtColor(self.__img, cv2.COLOR_BGR2GRAY)
         height = gray.shape[0]
 
         axis_end_y = -1
-        for i in range(height - originHeight, 0, -1): # Spalte von da y-Achse wird von oben noch unten durch gonga
-            if gray[i, origin_x_pos] == 255: # Wonn d Achse aufhead, oiso da Grauwert s erste moi ned weiß is
+        for i in range(height + self.originHeight, 0, -1): # Spalte von da y-Achse wird von unten noch oben durch gonga (Werte umdraht weil links oben = 0/0, height + weil originHeight negativ)
+            if gray[i, origin_x_pos] == 255: # Wonn d Achse aufhead, oiso da Grauwert s erste moi weiß is
                 axis_end_y = i
                 break
 
-        axis_values = range(axis_end_y, height - (originHeight + 1))
+        axis_values = range(axis_end_y, height - (self.originHeight + 1))
 
         return axis_values
 
-    def __getYAxisValuesOffset(img):
-        values = __getYAxisValues(img)
-        for i in len(values):
-            values[i] += 72
+    def getYAxisValuesOffset(self):
+        values = self.getYAxisValues()
+        for i in values:
+            i = i - self.originHeight
         return values
 
 
@@ -86,11 +90,14 @@ class LogAxis:
 # 9
 
 # x = 100
+
+
 # hobs do eine do, damits nd beim import ausgführt wird
 if __name__ == "__main__":
     for root, dirs, files in os.walk('../docs/Beispiele'):
         for filename in files:
             if filename.endswith('.png'):
-                img = cv2.imread(os.path.join(root, filename))
-                yValues = getYAxisValues(img)
+                imgPath = os.path.join(root, filename)
+                axis = LogAxis(imgPath)
+                yValues = axis.getYAxisValues()
                 print('Y-Axis in image ' + filename + ' is represented by the following pixels: ' + str(yValues))
