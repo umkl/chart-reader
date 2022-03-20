b4b2a29f1b2e933c43877d71fbd1cda35c12cf4a
ungarmichael <ungarmichael.mail@gmail.com>
2022-03-20 22:52:38 +0100

    new log consisting of all compartments 🤠

--- chartreader/result.py
+++ chartreader/result.py
@@ -1,3 +1,5 @@
+from distutils.log import Log
+from sqlite3 import Date
 from xaxis import DateAxis
 from yaxis import LogAxis
 from chart import Chart
@@ -5,13 +7,29 @@ import csv
 
 
 class Result:
-    def __init__(self, chart, dateaxis, logaxis):
+    def __init__(self, chart: Chart, dateaxis: DateAxis, logaxis: LogAxis):
         self.__dateAxis = dateaxis
         self.__logAxis = logaxis
         self.__chart = chart
+        self.__dateMapped = [] # array consisting of: key: pixelValX - value: date
+        self.__logMapped = [] # array consisting of: key: pixelValY - value: log
+        self.__fullMapped = [] # chart pixelwerte - abgezogen pixelwerte - auf dateaxis und logaxis gemapped 
+        
+
+    def mapOlles(self):
+        for index, value in self.__dateAxis.values:
+            self.__dateMapped[index] = value
+        for index, value in self.__logAxis.values:
+            self.__logMapped[index] = value
+        
+        for x, y in self.__chart.coordinates: # x ought to be the key and y the value
+            self.__fullMapped.append([self.__dateMapped[x],self.__logMapped[y]])
+            
 
     def logToCsv(self):
         with open('output/1.csv', 'w') as f:
             writer = csv.writer(f)
-            for value in self.__chart.getConverted():
+            # for value in self.__chart.getConverted():
+            #     writer.writerow(value)
+            for value in self.__fullMapped():
                 writer.writerow(value)
